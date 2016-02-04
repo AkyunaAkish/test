e583c8e003132d07215dc8333637e2437dfebf59
AkyunaAkish <akyunaakish@gmail.com>
2016-02-04 12:54:36 -0700

    testing

---
+++ test.js
@@ -0,0 +1,13 @@
+var a = ['hello', 'there', 'world'];
+
+function test(arr){
+
+  for(var i = 0;i<arr.length;i++){
+    console.log(arr[i]);
+  }
+
+  return 'yoyoyo';
+  
+}
+
+console.log(test(a));
